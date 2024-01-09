d459a5e21c58e42d71bfbec8cdbce635bbbaa7fb
rhele shirazi <124025212+raheleshirazi77@users.noreply.github.com>
2024-01-10 02:13:41 +0330

    createbody page
    * add page html and css
    * add page js factorial

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+name= rahele shirazi;
+term= javascript 1;
+project= daining factorial number;
+email=rahele.shirazi77@gmail.com;
+address github=raheleshirazi77
+ */
+
+// start project
+function factorial(n) { //tarif function from gaining number factorial
+    let result = 1; //tarif yek motakhayer from gaining number factorial
+    for (let i = 1; i <= n; i++) { // from gaining enters user number 
+      result *= i; // do the operation factorial
+    }
+    return result; //return answer the operation
+  }
+  
+document.write('facoraial: ',factorial(prompt("enter your number"))); //Eventually display the operation 
+  //tamaaaaam tammmaaam👌😊
+ 
+  
